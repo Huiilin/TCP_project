e2976b090ab820331986f9fb4764f004aa770b39
Huilin <74600403+Huiilin@users.noreply.github.com>
2021-05-21 20:24:27 +0800

    Create 4_KNN_imputation.R

---
+++ Transcriptome/4_KNN_imputation.R
@@ -0,0 +1,25 @@
+rm(list = ls())
+options(scipen = 200)
+
+load("3_RNA_V2/gene_FPKM_54_outliers2na.RData")
+
+library(impute)
+library(tidyverse)
+
+## impute.KNN requires input data samples in columns, genes in rows
+## our input data: sample in rows 
+## so need to t(data) first
+cols = str_subset(colnames(RNA_outliers2na),pattern = "ENSG")
+
+imputeDat <- t(RNA_outliers2na[,cols]) %>% as.data.frame
+
+imputeDat1 <- apply(imputeDat,2,as.numeric)  %>% as.data.frame
+rownames(imputeDat1) = rownames(imputeDat)
+
+## impute
+aa = impute.knn(as.matrix(imputeDat1), maxp = 50000)
+imputeDat2 <- aa$data
+
+table(is.na(imputeDat2))
+
+save(imputeDat2,file = "3_RNA_V2/gene_FPKM_54_KNNimputed.RData")
