cd6df68ebff24bbef23f50e4f9d3d99536562514
Huilin <74600403+Huiilin@users.noreply.github.com>
2021-08-05 20:36:33 +0800

    Update 2_deleteOutlierSamples.R

--- Transcriptome/2_deleteOutlierSamples.R
+++ Transcriptome/2_deleteOutlierSamples.R
@@ -6,14 +6,12 @@ library(tidyverse)
 library(data.table)
 library(readxl)
 
-load("3_RNA_V2/Trans_medinfo.RData")
+load("Trans_medinfo.RData")
 
 ## read in FPKM data
-file = "3_RNA_V2/gene_sample_FPKM.xlsx"
+file = "gene_sample_FPKM.xlsx"
 FPKM <- readxl::read_excel(path = file, sheet = 1,col_names = TRUE) %>% as.data.frame
 rownames(FPKM) <- unlist(FPKM$geneid)
-
-save(FPKM, file = "3_RNA_V2/gene_FPKM.RData")
 
 FPKM <- FPKM[,-1]
 FPKM <- FPKM[,rownames(medInfoTrans)]
@@ -34,4 +32,4 @@ medInfoTrans <- medInfoTrans[c(-55,-41,-56),]
 
 all(rownames(medInfoTrans) == colnames(FPKM54))
 
-save(FPKM54,medInfoTrans,file = "3_RNA_V2/gene_FPKM_54.RData")
+save(FPKM54,medInfoTrans,file = "gene_FPKM_54.RData")
